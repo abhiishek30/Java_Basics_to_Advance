b26cd546a931bb5309709e4cb1eac8260958c528
abhiishek30 <abhisheksingh8356@gmail.com>
2026-06-09 11:11:44 +0530

    initial commit

---
+++ src/multiply.java
@@ -0,0 +1,7 @@
+public class multiply {
+    public static void main(String[] args){
+        System.out.println("2 of 2 ");
+        short n=2;
+        System.out.println(n*n);
+    }
+}
---
+++ src/sumofnumbers.java
@@ -0,0 +1,10 @@
+public class sumofnumbers {
+    public static void main(String[] args){
+        System.out.println("sum of three numbers");
+        int n1=12;
+        int n2=13;//asgfgdfgkgf
+        int n3=23;
+        int sum = n1+n2+n3;
+        System.out.print(sum);
+    }
+}
