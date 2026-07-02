b41082d2747f136321f63521e37dd05e97107e3d
abhiishek30 <abhisheksingh8356@gmail.com>
2026-07-02 21:48:08 +0530

    commit

---
+++ java_programs/basicsquare.java
@@ -0,0 +1,8 @@
+public class basicsquare {
+    public static void main(String[] args) {
+        float a =2.2f;
+        int b=-5;
+        float c = (a*a) + (b*b);
+        System.out.println(c);
+    }
+}
