46e0e8a196969a622325c85ea29278bbe0855644
abhiishek30 <abhisheksingh8356@gmail.com>
2026-07-03 22:05:21 +0530

    commit

---
+++ java_programs/basiccube.java
@@ -0,0 +1,13 @@
+//find a3+b3+c3;
+
+public class basiccube {
+    public static void main(String[] args) {
+        float a =2.24538f;
+        float b = 0.335354f;
+        float c = 0.23228f;
+        float d = (a*a*a) + (b*b*b) + (c*c*c);
+        System.out.println(d);
+    }
+}
+
+
